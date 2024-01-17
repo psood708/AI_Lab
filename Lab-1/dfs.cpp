63119bbe290fc5cdb6220a5795cf6d1cc078466b
Parth <parth.sce21@sot.pdpu.ac.in>
2024-01-17 10:51:26 +0530

    Updated DFS and BFS

--- Lab-1/dfs.cpp
+++ Lab-1/dfs.cpp
@@ -1,68 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Graph {
-public:
-    int vertices;
-    vector<vector<char>> adjacencyList;
+void addEdge(map<int, vector<int>>& adj, int v, int w) {
+    adj[v].push_back(w);
+}
 
-    Graph(int v) {
-        vertices = v;
-        adjacencyList.resize(vertices);
-    }
+void DFS(map<int, bool>& visited, const map<int, vector<int>>& adj, int v) {
+    visited[v] = true;
+    cout << v << " ";
 
-    void addEdge(char u, char v) {
-        adjacencyList[u - 'A'].push_back(v);
-        adjacencyList[v - 'A'].push_back(u);  // For undirected graph
-    }
-
-    void DFS(char startVertex) {
-        vector<bool> visited(vertices, false);
-        vector<char> traversalOrder;  // To store the traversal order
-        stack<char> s;
-
-        s.push(startVertex);
-
-        while (!s.empty()) {
-            char currentVertex = s.top();
-            s.pop();
-
-            if (!visited[currentVertex - 'A']) {
-                cout << currentVertex << " ";
-                traversalOrder.push_back(currentVertex);
-                visited[currentVertex - 'A'] = true;
-            }
-
-            for (char neighbor : adjacencyList[currentVertex - 'A']) {
-                if (!visited[neighbor - 'A']) {
-                    s.push(neighbor);
-                }
-            }
-        }
-
-        cout << "\nTraversal Order: ";
-        for (char vertex : traversalOrder) {
-            cout << vertex << " ";
+    for (int i : adj.at(v)) {
+        if (!visited[i]) {
+            DFS(visited, adj, i);
         }
     }
-};
+}
 
 int main() {
-    // Example Usage
-    Graph g(11);
-    g.addEdge('S', 'A');
-    g.addEdge('S', 'H');
-    g.addEdge('A', 'B');
-    g.addEdge('A', 'C');
-    g.addEdge('H', 'I');
-    g.addEdge('H', 'J');
-    g.addEdge('B', 'D');
-    g.addEdge('B', 'E');
-    g.addEdge('C', 'G');
-    g.addEdge('I', 'K');
+    map<int, vector<int>> adj;
+    map<int, bool> visited;
 
-    cout << "Depth-First Search starting from vertex 'S': ";
-    g.DFS('S');
+    // Create a graph given in the above diagram
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 1, 4);
+    addEdge(adj, 3, 7);
+    addEdge(adj, 2, 5);
+    addEdge(adj, 2, 6);
+    addEdge(adj, 5, 8);
+
+    cout << "Following is Depth First Traversal (starting from vertex 2) \n";
+
+    // Function call
+    DFS(visited, adj, 0);
 
     return 0;
 }
---
+++ Lab-2/dfid.cpp
@@ -0,0 +1,1 @@
+// Depth first iterative deepening
